48b609007d5d539421693f4066cdd326ad3268f1
2503010119-Muhammad-Alfa-Raihani <2503010119@unper.ac.id>
2026-06-19 21:36:34 +0700

    Update perpustakaan.h

--- perpustakaan.h
+++ perpustakaan.h
@@ -1,25 +1,11 @@
 #ifndef PERPUSTAKAAN_H
 #define PERPUSTAKAAN_H
 
-#include <vector>
-#include "Buku.h"
-
-using namespace std;
-
 class Perpustakaan {
-private:
-    vector<Buku> daftarBuku;
-
 public:
-    // Kelola Buku
+    void menuPerpustakaan();
     void tambahBuku();
-    void tampilkanBuku();
-
-    // Fitur anggota lain
-    void pinjamBuku();
-    void kembalikanBuku();
-    void cariBuku();
-    void rekomendasiBuku();
+    void lihatBuku();
 };
 
 #endif
