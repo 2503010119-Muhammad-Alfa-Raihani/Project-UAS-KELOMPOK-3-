8ebb82fb1cecc6001100d1c53f3642ff1a3d1f9d
2503010112-Muhammad-Rasya-Aditya-R <2503010112@unper.ac.id>
2026-06-19 13:30:07 +0700

    Rekomendasi.h

--- Rekomendasi.h
+++ Rekomendasi.h
@@ -1,1 +1,20 @@
+#ifndef PERPUSTAKAAN_H
+#define PERPUSTAKAAN_H
 
+#include <vector>
+#include "Buku.h"
+using namespace std;
+
+class Perpustakaan {
+private:
+    vector<Buku> daftarBuku;
+
+public:
+    void tambahBuku(Buku buku);
+    void tampilkanBuku();
+
+    // Fungsi rekomendasi buku
+    void rekomendasiBuku();
+};
+
+#endif
