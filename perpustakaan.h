c436c620a1e40bf6ff3560c33eed233d33d185d1
2503010119-Muhammad-Alfa-Raihani <2503010119@unper.ac.id>
2026-07-02 18:18:22 +0700

    Enhance Perpustakaan class with additional methods
    Added new methods for book management and search functionality.

--- perpustakaan.h
+++ perpustakaan.h
@@ -1,11 +1,46 @@
 #ifndef PERPUSTAKAAN_H
 #define PERPUSTAKAAN_H
 
+#include <string>
+#include <iostream>
+#include "Buku.h"
+
+using namespace std;
+
+#define MAX_BUKU 100
+
 class Perpustakaan {
+private:
+    Buku daftarBuku[MAX_BUKU];
+    int jumlahBuku;
+
 public:
+    Perpustakaan();
+    
+    // Menu
     void menuPerpustakaan();
-    void tambahBuku();
+    
+    // Pengelolaan Buku
+    void tambahBuku(Buku buku);
+    void tampilkanBuku();
     void lihatBuku();
+    
+    // Pencarian
+    void cariJudul(string judul);
+    void cariPenulis(string penulis);
+    void cariKategori(string kategori);
+    
+    // Peminjaman & Pengembalian
+    bool pinjamBuku(int idBuku);
+    bool kembalikanBuku(int idBuku);
+    
+    // Rekomendasi
+    void rekomendasiBuku();
+    
+    // Getter
+    Buku* getBuku(int index);
+    int getJumlahBuku();
+    Buku* cariBukuById(int id);
 };
 
 #endif
