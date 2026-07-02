d1d172ee06e2443e90798e73de5255288c17a22d
2503010119-Muhammad-Alfa-Raihani <2503010119@unper.ac.id>
2026-07-02 18:00:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,37 +2,62 @@
 #include "perpustakaan.h"
 #include "peminjamanbuku.h"
 #include "Rekomendasi.h"
+#include "Pencarian.h"
+#include "pengembalian.h"
 
 using namespace std;
 
 int main() {
-    Perpustakaan p;
-    PeminjamanBuku pb;
-    Rekomendasi r;
+    Perpustakaan perpus;
+    PeminjamanBuku pb(&perpus);
+    Rekomendasi r(&perpus);
+    Pencarian pc(&perpus);
+    Pengembalian pg(&perpus);
 
     int pilihan;
 
     do {
-        cout << "\n=== SISTEM PERPUSTAKAAN ===\n";
+        cout << "\n============================================\n";
+        cout << "     SISTEM MANAJEMEN PERPUSTAKAAN MODERN\n";
+        cout << "============================================\n";
         cout << "1. Kelola Buku\n";
         cout << "2. Peminjaman Buku\n";
-        cout << "3. Rekomendasi\n";
+        cout << "3. Pengembalian Buku\n";
+        cout << "4. Pencarian Buku\n";
+        cout << "5. Rekomendasi Buku\n";
         cout << "0. Keluar\n";
-        cout << "Pilih: ";
+        cout << "============================================\n";
+        cout << "Pilih menu: ";
         cin >> pilihan;
+        cin.ignore();
 
         switch(pilihan) {
             case 1:
-                p.menuPerpustakaan();
+                perpus.menuPerpustakaan();
                 break;
 
             case 2:
                 pb.menuPeminjaman();
                 break;
-
+                
             case 3:
+                pg.menuPengembalian();
+                break;
+                
+            case 4:
+                pc.menuPencarian();
+                break;
+
+            case 5:
                 r.menuRekomendasi();
                 break;
+                
+            case 0:
+                cout << "\nTerima kasih telah menggunakan sistem perpustakaan!\n";
+                break;
+                
+            default:
+                cout << "\nPilihan tidak valid!\n";
         }
 
     } while(pilihan != 0);
