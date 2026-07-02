3257050f343dd2156c17c2566ab285a438b2a6d9
2503010119-Muhammad-Alfa-Raihani <2503010119@unper.ac.id>
2026-07-02 18:15:45 +0700

    Implement PeminjamanBuku class in peminjamanbuku.h
    Added PeminjamanBuku class for book borrowing functionality.

--- peminjamanbuku.h
+++ peminjamanbuku.h
@@ -7,7 +7,29 @@
 
 class Perpustakaan {
 private:
-    std::vector<Buku> daftarBuku; 
+    std::vector<Buku> daftarBuku; #ifndef PEMINJAMANBUKU_H
+#define PEMINJAMANBUKU_H
+
+#include <iostream>
+#include <string>
+#include "perpustakaan.h"
+
+using namespace std;
+
+class PeminjamanBuku {
+private:
+    Perpustakaan* perpus;
+
+public:
+    PeminjamanBuku(Perpustakaan* p = nullptr);
+    
+    void menuPeminjaman();
+    void pinjamBukuMenu();
+    void lihatBukuTersedia();
+};
+
+#endif
+
 
 public:A
     
